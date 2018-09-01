a7835c76fa62b197eb7ee3b3ef311d051a9bf648
Jacob Louis <jacob@bluelotusmedia.com>
2018-09-01 17:05:58 -0600

    Adds magenta music demo example

--- simple-react-full-stack/src/client/App.js
+++ simple-react-full-stack/src/client/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import './app.css';
+import * as mm from '@magenta/music';
 
+// Instantiate model by loading desired config.
+  const model = new mm.MusicVAE( 'https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_small_q2');
+  const player = new mm.Player();
+
+  function playTune() {
+    mm.Player.tone.context.resume();  // enable audio
+    model.sample(1)
+      .then((samples) => player.start(samples[0], 100));
+  }
 
 export default class App extends Component {
     
@@ -40,6 +50,7 @@ export default class App extends Component {
       <div>
         {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
         <button onClick={this.midiGen.bind(this)}>Generate Midi</button>
+        <button onClick={playTune}><h1>Play Trio</h1></button>
         
         <div>test {this.state.success}</div>
       </div>
