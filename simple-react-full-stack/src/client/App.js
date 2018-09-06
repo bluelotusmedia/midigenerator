5d60a925ad4f2c250e3d47d62e4931d6e406e1df
Jacob Louis <jacob@bluelotusmedia.com>
2018-09-05 23:31:19 -0600

    Adds more options that get passed into the scribbletune api.

--- simple-react-full-stack/src/client/App.js
+++ simple-react-full-stack/src/client/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import './app.css';
-import * as mm from '@magenta/music';
 import Button from '@material-ui/core/Button';
 import NavBar from './components/NavBar';
-import DownloadLink from "react-download-link";
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import green from '@material-ui/core/colors/green';
@@ -21,7 +19,7 @@ const theme = createMuiTheme({
 });
 
 // sequencer settings
-const stepNum = 16;
+//const stepNum = 16;
 
 
 export default class App extends Component {
@@ -29,19 +27,42 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: null,
+      stepNumber: 16,
       success: 'false',
       fileName: null,
-      steps: Array(stepNum).fill('-')
+      keys: ['Random','C','C#','D','Eb','E','F','F#','G','Ab','A','Bb','B'],
+      currentKey: 'Random',
+      modes: ['Random','Major','Minor'],
+      currentMode: 'Random',
+      progs: ["Random",
+              "I,IV,V", 
+              "I,vi,IV,V", 
+              "ii,V,I", 
+              "I,vi,ii,V", 
+              "I,V,vi,IV", 
+              "I,IV,vi,V",
+              "I,IV,V,ii",
+              "IV,ii,V,I",
+              "IV,I7,ii7"],
+      currentProg: 'Random',
+      divs:["Random","1n","2n","3n","4n","8n","16n"],
+      currentDiv: 'Random',
+      steps: Array(16).fill('-')
     };
-    
+      
+    this.patternLength = this.patternLength.bind(this);
   }
     
   midiGen(settings) {
-    
-    const patternString = settings.join("");
       
-    fetch('/api/midiGen?settings='+patternString)
+    let notes = this.state.keys;
+    let pattern = settings[0].join("");  
+    let key = this.state.keys.indexOf(settings[1]);
+    let mode = settings[2];
+    let prog = settings[3];
+    let div = settings[4];
+      
+    fetch('/api/midiGen?pattern='+pattern+'&key='+key+'&mode='+mode+'&prog='+prog+'&div='+div)
     .then((success, req) => success.json())
     .then((success, req) => {
         
@@ -54,8 +75,8 @@ export default class App extends Component {
 
         }
     })
-    .catch(function(error) {
-      console.log(error)
+    .catch((error) => {
+      this.setState({message: error.message+' Please try again.'});
     }); 
      
   }
@@ -66,14 +87,42 @@ handleStepClick(i) {
     let steps = this.state.steps.slice(0);
     if (steps[i] == '-') {
         steps[i] = 'x'
+    } else if (steps[i] == 'x') {
+        steps[i] = '_'
     } else {
         steps[i] = '-'
     };
+    
     this.setState({steps: steps})
+}
+    
+patternLength(e) { 
+    this.setState({stepNumber: e.target.value});
+    this.setState({steps: Array(Number(e.target.value)).fill('-')});
+
+}
+    
+changeKey(e) { 
+    let newKey = e.target.value;
+    this.setState({currentKey: newKey});
+}
+
+changeMode(e) { 
+    let newMode = e.target.value;
+    this.setState({currentMode: newMode});
+}
+
+changeProg(e) { 
+    let newProg = e.target.value;
+    this.setState({currentProg: newProg});
+}
+
+changeDiv(e) { 
+    let newDiv = e.target.value;
+    this.setState({currentDiv: newDiv});
 }
 
   render() {
-    const { username } = this.state;
    
     return (
       <MuiThemeProvider theme={theme}>
@@ -81,15 +130,46 @@ handleStepClick(i) {
     
         <p>{this.state.message}</p>
         
+       Steps (clears pattern): <input onKeyUp={this.patternLength.bind(this)} defaultValue={this.state.stepNumber} size={4} />
+    
+        Key: <select onChange={this.changeKey.bind(this)}>
+           {this.state.keys.map((key, i) => {
+            return <option key={ i }>{key}</option>;
+           })}
+        </select>
+        
+        Mode: <select onChange={this.changeMode.bind(this)}>
+           {this.state.modes.map((mode, i) => {
+            return <option key={ i }>{mode}</option>;
+           })}
+        </select>
+        
+        Progression: <select onChange={this.changeProg.bind(this)}>
+           {this.state.progs.map((prog, i) => {
+            return <option key={ i }>{prog}</option>;
+           })}
+        </select>
+        
+        Divisions: <select onChange={this.changeDiv.bind(this)}>
+           {this.state.divs.map((div, i) => {
+            return <option key={ i }>{div}</option>;
+           })}
+        </select>
+        
         <div style={{ margin: "0 0 20px 5px",
             textDecoration: "none",
             cursor: "pointer"}}>
            {this.state.steps.map((step, i) => {
-            return <button key={ i } onClick={this.handleStepClick.bind(this, i)}>{step}</button>;
+            return <Button key={ i } onClick={this.handleStepClick.bind(this, i)} variant="contained" color="secondary">{step}</Button>;
            })}
         </div>
         
-        <Button variant="contained" color="secondary" onClick={this.midiGen.bind(this, this.state.steps)}>
+        <Button variant="contained" color="secondary" onClick={this.midiGen.bind(this, [this.state.steps, 
+         this.state.currentKey,
+         this.state.currentMode,
+         this.state.currentProg,
+         this.state.currentDiv
+        ])}>
             Generate Midi
         </Button>
         
--- simple-react-full-stack/src/server/index.js
+++ simple-react-full-stack/src/server/index.js
@@ -8,61 +8,95 @@ app.use(express.static('dist'));
 app.listen(8080);
 
 app.get('/api/midiGen', (req, res) => {
-    let pattern = req.query.settings;
-    let success = midiGen(pattern);
+    let pattern = req.query.pattern;
+    let key = req.query.key;
+    let mode = req.query.mode;
+    let prog = req.query.prog;
+    let div = req.query.div;
+    
+    let success = midiGen(pattern,key,mode,prog,div);
     res.send({ success });
 });
 
-function midiGen(pattern) {
+function midiGen(pattern,key,mode,prog,div) {
     // start scribbletune
-    var date = new Date();
-    var time = date.getTime();
-    //var modes = scribble.modes();
+    let date = new Date();
+    let time = date.getTime();
+    //let modes = scribble.modes();
     let modes = ['minor','major'];
-    var notes = ["c", "d", "e", "f", "g", "a", "b"]
+    let notes = ['c','c#','d','eb','e','f','f#','g','ab','a','bb','b']
+    let progs = ["I,IV,V", 
+                 "I,vi,IV,V", 
+                 "ii,V,I", 
+                 "I,vi,ii,V", 
+                 "I,V,vi,IV", 
+                 "I,IV,vi,V",
+                 "I,IV,V,ii",
+                 "IV,ii,V,I",
+                 "IV,I7,ii7"];
+    let divs = ["1n","2n","4n","8n","16n"];
 
     // random seed number
     let rootNum = Math.floor(Math.random()*9);
-
-    // random scale
-    modes.sort(function(a, b){return 0.5 - Math.random()});
-
-    // random notes
-    notes.sort(function(a, b){return 0.5 - Math.random()});
-
+    
+    if (pattern.indexOf('x')==-1) {
+        pattern = genPattern(rootNum);
+    } else {
+        // pattern is defined
+    }
+    
+    if (key == 0) {
+        // random key
+        notes.sort(function(a, b){return 0.5 - Math.random()});
+        key = notes[0];
+    } else {
+        key = notes[Number(key)-1];
+    }
+    
+    if (mode == 'Random') {
+        // random scale
+        modes.sort(function(a, b){return 0.5 - Math.random()});
+        mode = modes[0];
+    } else {
+        // mode is set
+        mode = mode.toLowerCase();
+    }
+    
+    if (prog == 'Random') {
+        // random progression
+       progs.sort(function(a, b){return 0.5 - Math.random()});
+       prog = progs[0];
+    } else {
+        // progression is set
+    }
+    
+    if (div == 'Random') {
+        // random progression
+       divs.sort(function(a, b){return 0.5 - Math.random()});
+       div = divs[0];
+    } else {
+        // progression is set
+        
+    }
+    console.log(div);
     genClip(4)
 
     function genClip(rootNum) {
         
         let clip = scribble.clip({
-            notes: scribble.progression(notes[0]+' '+modes[0], chooseProgression(rootNum)),
+            notes: scribble.progression(key+' '+mode, prog),
             pattern: pattern,
-            subdiv: '3n'
+            subdiv: div
         });
-
-        scribble.midi(clip, "scribbletune-"+notes[0]+"-"+modes[0]+"-"+time+".mid"); 
+        
+        // convert # to sharp for filename
+        var regex = /#/gi;
+        key = key.replace(regex, '-sharp');
+        
+        scribble.midi(clip, "scribbletune-"+key+"-"+mode+"-"+time+".mid"); 
     }
 
-    function chooseProgression(rootNum) {
-
-        let commonProgressions = ["I, IV, V", 
-                                  "I, vi, IV, V", 
-                                  "ii, V, I", 
-                                  "I, vi, ii, V", 
-                                  "I, V, vi, IV", 
-                                  "I, IV, vi, V",
-                                  "I, IV, V, ii",
-                                  "IV, ii, V, I",
-                                  "IV, I7, ii7"];
-
-        let maxlen = commonProgressions.length; 
-
-        commonProgressions.sort(function(a, b){return 0.5 - Math.random()});
-
-        return commonProgressions[0];
-    }
-
-
+    
 
 
     function isOdd(num) { return num % 2;}
@@ -92,8 +126,8 @@ function midiGen(pattern) {
     let success = [
         {
           "response": {
-            "fileName": "scribbletune-"+notes[0]+"-"+modes[0]+"-"+time+".mid",
-            message: 'chords in '+notes[0]+' '+modes[0]+' generated with a seed of '+rootNum+'!',
+            "fileName": "scribbletune-"+key+"-"+mode+"-"+time+".mid",
+            message: 'chords in '+key+' '+mode+' generated with a seed of '+rootNum+'!',
             notes, 
             modes, 
             rootNum
