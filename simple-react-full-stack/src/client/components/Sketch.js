41b85b885accb1a26e1299e19c5f4ff51b1100e0
Jacob Louis <jacob@bluelotusmedia.com>
2018-10-04 19:56:24 -0600

    Adds P5 support to the chord generation.

--- simple-react-full-stack/src/client/components/Sketch.js
+++ simple-react-full-stack/src/client/components/Sketch.js
@@ -8,19 +8,65 @@ export default function sketch(p) {
     let b = 100;
     let a = 25;
     let generatedProg = [];
-    
+
     p.setup = function () {
         p.createCanvas(1140, 425);
         p.background(0);
         p.angleMode(p.DEGREES);
         p.colorMode(p.HSB, 100);
     };
-    
+
     p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
-        if (props.generatedProg){
-          generatedProg = props.generatedProg;
+        if (props.generatedProg) {
+
+            generatedProg = props.generatedProg;
+            if (typeof generatedProg == "string") {
+                generatedProg = generatedProg.split(',');
+
+                generatedProg = generatedProg.map(item => {
+                    let thisItem;
+                    switch (item) {
+                        case "I":
+                        case "i":
+                            thisItem = 1;
+                            break;
+                        case "II":
+                        case "ii":
+                            thisItem = 2;
+                            break;
+                        case "III":
+                        case "iii":
+                            thisItem = 3;
+                            break;
+                        case "IV":
+                        case "iv":
+                            thisItem = 4;
+                            break;
+                        case "V":
+                        case "v":
+                            thisItem = 5;
+                            break;
+                        case "Vi":
+                        case "vi":
+                            thisItem = 6;
+                            break;
+                        case "VII":
+                        case "vii":
+                        case "I7":
+                        case "ii7":
+                            thisItem = 7;
+                            break;
+
+                    }
+
+                    return thisItem;
+
+                });
+            }
+
+
+            console.log(generatedProg)
         }
-        console.log(generatedProg)
     };
 
     p.draw = function () {
@@ -30,19 +76,19 @@ export default function sketch(p) {
         let yArray = [];
 
         p.translate(p.width / 2, p.height / 2);
-        p.fill(h,s,50,a)
-        p.stroke(h,s,100,100);
+        p.fill(h, s, 50, a)
+        p.stroke(h, s, 100, 100);
         p.rotate(angle);
         p.ellipse(0, 0, radius, radius);
 
         for (let i = 0; i <= 360 / rotation; i++) {
-            
+
             xArray.push(x);
             yArray.push(y);
             p.line(
-                xArray[generatedProg[i * 1 % generatedProg.length]] / 2, 
-                yArray[generatedProg[i * 1 % generatedProg.length]] / 2, 
-                xArray[i] / 2, 
+                xArray[generatedProg[i * 1 % generatedProg.length]] / 2,
+                yArray[generatedProg[i * 1 % generatedProg.length]] / 2,
+                xArray[i] / 2,
                 yArray[i] / 2);
             angle += rotation;
             x = radius * p.cos(angle);
@@ -50,8 +96,7 @@ export default function sketch(p) {
 
         }
         angle = p.round(angle % 360);
-        h = (h + inc) % 100; 
-        console.log(h)
-       
+        h = (h + inc) % 100;
+
     };
 };
